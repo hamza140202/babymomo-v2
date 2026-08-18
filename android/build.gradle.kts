ea2921e4f7e366ab9d9a7eacf3f88ad04d64c9d7
Bilal140202 <bilalansari1402@gmail.com>
2026-08-18 13:35:27 +0530

    fix(android): enforce compileSdk 35 and minSdk on all gradle subprojects

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,8 +16,25 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    afterEvaluate {
+        if (project.plugins.hasPlugin("com.android.library") || project.plugins.hasPlugin("com.android.application")) {
+            val android = project.extensions.findByName("android")
+            if (android is com.android.build.gradle.BaseExtension) {
+                android.compileSdkVersion(35)
+                android.defaultConfig {
+                    if (minSdk == null || minSdk!! < 24) {
+                        minSdk = 24
+                    }
+                }
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
